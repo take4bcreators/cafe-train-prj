2db81b63872555075bccf4477c2c0de584b0bdde
penguin <penguin@example.com>
2022-01-16 16:32:24 +0900

    テスト用ファイルの追加 #6

--- batch/shell/create_cont_csv/common_test_shell.sh
+++ batch/shell/create_cont_csv/common_test_shell.sh
@@ -3,49 +3,50 @@
 # シェルの名前を格納
 EXEC_SHELL_NAME=$(basename $0)
 
+### 共通設定読み込み ###
 # プロジェクトパス情報読み込みファイルの存在チェックと読み込み
 if [ ! -f prjrootpath.sh ]; then
-    echo "プロジェクトパス情報読み込みファイルが存在しません"
+    echo "ERROR プロジェクトパス情報読み込みファイルが存在しません"
     exit 1
 fi
 source prjrootpath.sh
 
 # 共通設定読み込みファイルの存在チェックと読み込み
-if [ ! -f $PROJECT_ROOT/config/common_env.sh ]; then
-    echo "共通設定読み込みファイルが存在しません"
+if [ ! -f ${PROJECT_ROOT}/config/common_env.sh ]; then
+    echo "ERROR 共通設定読み込みファイルが存在しません"
     exit 1
 fi
-source $PROJECT_ROOT/config/common_env.sh
+source ${PROJECT_ROOT}/config/common_env.sh
 
 # 機能別設定読み込みファイルの存在チェックと読み込み
-GRP_ENV_FILE_NAME=grp_env_create_cont_csv.sh
-if [ ! -f $CONFIG_DIR/$GRP_ENV_FILE_NAME ]; then
-    echo "機能別設定読み込みファイルが存在しません"
+grp_env_file_name=grp_env_create_cont_csv.sh
+if [ ! -f ${CONFIG_DIR}/${grp_env_file_name} ]; then
+    echo "ERROR 機能別設定読み込みファイルが存在しません"
     exit 1
 fi
-source $CONFIG_DIR/$GRP_ENV_FILE_NAME
+source ${CONFIG_DIR}/${grp_env_file_name}
 
 # 共通モジュール読み込みファイルの存在チェックと読み込み
-if [ ! -f $CONFIG_DIR/common_lib.sh ]; then
-    echo "共通モジュール読み込みファイルが存在しません"
+if [ ! -f ${CONFIG_DIR}/common_lib.sh ]; then
+    echo "ERROR 共通モジュール読み込みファイルが存在しません"
     exit 1
 fi
-source $CONFIG_DIR/common_lib.sh
+source ${CONFIG_DIR}/common_lib.sh
 
 # 認証情報読み込みファイルの存在チェックと読み込み
-if [ ! -f $ENV_DIR/.env ]; then
-    echo "認証情報読み込みファイルが存在しません"
+if [ ! -f ${ENV_DIR}/.env ]; then
+    echo "ERROR 認証情報読み込みファイルが存在しません"
     exit 1
 fi
-source $ENV_DIR/.env
+source ${ENV_DIR}/.env
 
 
-
-log_msg $INFO "実行開始"
+### メイン処理 ###
+log_msg ${INFO} "実行開始"
 
 echo "メイン処理"
-log_msg $INFO "メイン処理実行中"
+log_msg ${INFO} "メイン処理実行中"
 
-log_msg $INFO "実行完了"
+log_msg ${INFO} "実行完了"
 
 exit 0
