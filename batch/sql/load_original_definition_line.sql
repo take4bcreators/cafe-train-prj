2a75536fa1b7510bc1472fd507a33addd9f0b5ea
hdeg <76611318+exit1991@users.noreply.github.com>
2022-01-24 18:54:10 +0900

    SQLリファクタリング #17

--- batch/sql/load_original_definition_line.sql
+++ batch/sql/load_original_definition_line.sql
@@ -1,8 +1,8 @@
--- 処理対象情報
-\set to_table_name 'mst_original_definition_line'
-\set column_list 'line_code,line_definition_code,line_definition_name,line_definition_color'
-\set from_csv_path :project_root'/batch/csv/mst_original_definition_line.csv'
+-- 処理対象指定
+\set from_csv_path  :project_root'/batch/csv/mst_original_definition_line.csv'
+\set to_table_name  'mst_original_definition_line'
+\set column_list    'line_code,line_definition_code,line_definition_name,line_definition_color'
 
--- 読み込みコマンド組立・実行 (カンマ区切り・ヘッダーなし)
-\set copycmd '\\COPY ':schema'.':to_table_name'(':column_list') FROM \'':from_csv_path'\' WITH CSV'
-:copycmd
+-- 読込コマンド組立・実行 (カンマ区切り・ヘッダーなし)
+\set importcmd '\\COPY ':schema'.':to_table_name'(':column_list') FROM \'':from_csv_path'\' WITH CSV'
+:importcmd
--- batch/sql/load_target_prefectures.sql
+++ batch/sql/load_target_prefectures.sql
@@ -1,8 +1,8 @@
--- 処理対象情報
-\set to_table_name 'mst_target_prefectures'
-\set column_list 'target_prefecture_code'
-\set from_csv_path :project_root'/batch/csv/mst_target_prefectures.csv'
+-- 処理対象指定
+\set from_csv_path  :project_root'/batch/csv/mst_target_prefectures.csv'
+\set to_table_name  'mst_target_prefectures'
+\set column_list    'target_prefecture_code'
 
--- 読み込みコマンド組立・実行 (テキスト形式)
-\set copycmd '\\COPY ':schema'.':to_table_name'(':column_list') FROM \'':from_csv_path'\''
-:copycmd
+-- 読込コマンド組立・実行 (テキスト形式)
+\set importcmd '\\COPY ':schema'.':to_table_name'(':column_list') FROM \'':from_csv_path'\''
+:importcmd
