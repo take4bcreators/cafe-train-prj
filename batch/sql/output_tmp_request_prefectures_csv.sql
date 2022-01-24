769d5ab0fd99d356a0677f53ce9d8b5a6dd07857
hdeg <76611318+exit1991@users.noreply.github.com>
2022-01-24 18:48:12 +0900

    鉄道会社情報取得まで作成 #17

---
+++ batch/sql/output_tmp_request_prefectures_csv.sql
@@ -0,0 +1,8 @@
+-- 処理対象情報
+\set from_table_name 'mst_target_prefectures'
+\set column_list     'target_prefecture_code'
+\set to_csv_path     :project_root'/tmp/tmp_request_prefectures.csv'
+
+-- 出力コマンド組立・実行 (テキスト形式)
+\set outputcmd '\\COPY (SELECT ':column_list' FROM ':schema'.':from_table_name') TO \'':to_csv_path'\''
+:outputcmd
