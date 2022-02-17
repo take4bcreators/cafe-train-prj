7b11fc350215573ae1352aff778389a4715e95bf
hdeg <76611318+exit1991@users.noreply.github.com>
2022-02-17 19:35:03 +0900

    カフェチェーンマスタ修正 #39

--- config/grp_env_create_request.sh
+++ config/grp_env_create_request.sh
@@ -4,6 +4,7 @@ export MOD_GRP_NAME="create_request"
 #########################
 # ファイルパス定義
 #########################
+export GRP_CAFE_MST_CSV="${CSV_DIR}/mst_cafe_chain.csv"
 export GRP_REQUEST_REPORT="${DATA_DIR}/request_data_report.txt"
 
 #########################
@@ -11,3 +12,10 @@ export GRP_REQUEST_REPORT="${DATA_DIR}/request_data_report.txt"
 #########################
 export GRP_REQUEST_COST_YEN="4"
 
+#########################
+# PostgreSQLのバインド変数（共通設定に追記）
+# SQLでも環境変数を使用したい場合に指定する
+#########################
+export DB_BIND+=" \
+    -v GRP_CAFE_MST_CSV=${GRP_CAFE_MST_CSV} \
+"
