5ca924ba1fd11a7671be2279e5e1edc38b209812
hdeg <76611318+exit1991@users.noreply.github.com>
2022-02-19 22:58:48 +0900

    リクエスト用CSV出力に書込権付与を追加 #39

--- batch/sql/export_placesapi_request_data_csv.sql
+++ batch/sql/export_placesapi_request_data_csv.sql
@@ -1,7 +1,7 @@
 -- 処理対象指定
 \set from_table_name    'tmp_unique_fetch_lineinfo'
 \set column_list        'lat, lon'
-\set to_csv_path        :GRP_REQUEST_CSV
+\set to_csv_path        :SHL_EXPORT_FILE_PATH
 
 -- 出力コマンド組立・実行 (CSV／列指定あり／ヘッダーなし)
 \set exportcmd          '\\COPY (SELECT ':column_list' FROM ':schema'.':from_table_name') TO \'':to_csv_path'\' WITH CSV'
