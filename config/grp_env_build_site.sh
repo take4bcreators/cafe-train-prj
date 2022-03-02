3087b49c67a55ce3149724d7182ef066139a0321
hdeg <76611318+exit1991@users.noreply.github.com>
2022-03-02 21:27:12 +0900

    zip作成処理追加 #61

--- config/grp_env_build_site.sh
+++ config/grp_env_build_site.sh
@@ -6,6 +6,6 @@ export MOD_GRP_NAME="build_site"
 #########################
 export GRP_PAGE_CONT_CSV="${DATA_DIR}/page_contents.csv"
 export SSG_DATA_DIR="${SSG_DIR}/src/data"
-export SSG_PUBLIC_DIR="${SSG_DIR}/public"
+export SSG_PUBLIC_DIR_NAME="public"
 export BUILD_TMP_LOG_NAME="build_tmp.log"
-
+export SITE_DATA_ZIP="${DATA_DIR}/staticsite_data.zip"
