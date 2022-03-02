a418d4dfface3a7e5d8fe841d763d2872fa0f341
hdeg <76611318+exit1991@users.noreply.github.com>
2022-03-02 20:24:30 +0900

    コンテンツCSVコピー処理作成 #61

--- config/grp_env_build_site.sh
+++ config/grp_env_build_site.sh
@@ -1,2 +1,10 @@
 # 機能別設定
 export MOD_GRP_NAME="build_site"
+
+#########################
+# ファイルパス定義
+#########################
+export GRP_PAGE_CONT_CSV="${DATA_DIR}/page_contents.csv"
+export SSG_DATA_DIR="${SSG_DIR}/src/data"
+export SSG_PUBLIC_DIR="${SSG_DIR}/public"
+
