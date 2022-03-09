487e81dbfa586cbbaa1b00d7a24be9a66c9cf437
hdeg <76611318+exit1991@users.noreply.github.com>
2022-03-09 21:51:47 +0900

    プラグインインストール直後 #50

--- ssg/gatsby-config.js
+++ ssg/gatsby-config.js
@@ -6,5 +6,14 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `data`,
+        path: `${__dirname}/src/data/`,
+      },
+    },
+    `gatsby-transformer-csv`,
+  ],
 }
