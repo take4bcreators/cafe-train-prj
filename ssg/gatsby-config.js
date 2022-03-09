9654fa248273e7136039c0caad77bf323ce53ef2
hdeg <76611318+exit1991@users.noreply.github.com>
2022-03-09 21:44:11 +0900

    gatsby-starter-hello-world 再インストール直後 #50

--- ssg/gatsby-config.js
+++ ssg/gatsby-config.js
@@ -4,21 +4,7 @@
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
 
- module.exports = {
+module.exports = {
   /* Your site config here */
-  siteMetadata: {
-    title: `Cafein`,
-    description: `駅周辺のカフェ情報を一覧化したマップを公開するサイト`,
-    lang: `ja`,
-  },
-  plugins: [
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `data`,
-        path: `${__dirname}/src/data/`,
-      },
-    },
-    `gatsby-transformer-csv`,
-  ],
+  plugins: [],
 }
